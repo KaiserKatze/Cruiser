96c040c8e168a0e02b1d070ef1af5ba3c8c4b477
donizyo <donizyo@gmail.com>
2016-04-26 01:56:11 +0800

    Update note.sh
    Enrich my bash toolkit for developing with git and make

--- note.sh
+++ note.sh
@@ -6,9 +6,12 @@ git branch
 
 echo "\nMaking..."
 make > /dev/null
-echo
 
+echo "\nTasks:"
+find -type f ! -name "note.sh" -exec grep -n "TODO" {} + | sed -e 's/\.\///' -e 's/\s\+\/\/\s*TODO\s*/\t/' -e 's/include\///'
+
+echo
 git log --graph -8 --abbrev-commit --oneline
-echo
 
-git status
+echo "\nChanges not staged:"
+git status | grep "modified:" | sed -e 's/#\s\+modified:\s\+/\t/'
