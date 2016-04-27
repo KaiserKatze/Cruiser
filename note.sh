560a6433e6a9061b08238bd40a975ad97266958c
donizyo <donizyo@gmail.com>
2016-04-28 02:41:55 +0800

    Update note.sh

--- note.sh
+++ note.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+FILE_TODO="TODO.md"
+
 clear
 echo "Branches:"
 git branch
@@ -8,9 +10,12 @@ echo "\nMaking..."
 make > /dev/null
 
 echo "\nTasks:"
-find -type f ! -name "note.sh" -exec grep -n "TODO" {} + | sed -e 's/\.\///' -e 's/\s\+\/\/\s*TODO\s*/\t/' -e 's/include\///'
-
+find -type f ! -name "note.sh" ! -path "./.git*" ! -path "./build*" -exec grep -n "TODO" {} + | sed -e 's/\.\///' -e 's/\s\+\/\/\s*TODO\s*/\t/' -e 's/include\///'
 echo
+if [ -f $FILE_TODO ]; then
+    cat $FILE_TODO
+fi
+
 git log --graph -8 --abbrev-commit --oneline
 
 echo "\nChanges not staged:"
