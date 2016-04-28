ded83ad68eacf91e559a49ca8a25e71cbd785758
donizyo <donizyo@gmail.com>
2016-04-28 10:33:33 +0800

    Update note.sh

--- note.sh
+++ note.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 FILE_TODO="TODO.md"
+COLOR_NONE='\033[0m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[1;33m'
 
 clear
-echo "Current branch:"
-git branch | grep '*' | sed -e 's/\* /\t/'
+echo "${COLOR_RED}Branches:${COLOR_NONE}"
+git branch
 
-echo "\nMaking..."
+echo "\n${COLOR_RED}Making...${COLOR_NONE}"
 make > /dev/null
 
-echo "\nTasks:"
+echo "\n${COLOR_RED}Tasks:${COLOR_NONE}"
 find -type f ! -name "note.sh" ! -path "./.git*" ! -path "./build*" -exec grep -n "TODO" {} + | sed -e 's/\.\///' -e 's/\s\+\/\/\s*TODO\s*/\t/' -e 's/include\///'
 echo
 if [ -f $FILE_TODO ]; then
